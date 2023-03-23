d2d5c800793b7656016d277b02fd61e1e08d64d3
Jonathan_HA78 <joemaxwell789@gmail.com>
2023-03-23 21:09:50 +0700

    fixing json:data

--- model/response.go
+++ model/response.go
@@ -3,13 +3,13 @@ package model
 type UserResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
-	Data    User   `json:data`
+	Data    User   `json:"data"`
 }
 
 type UsersResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
-	Data    []User `json:data`
+	Data    []User `json:"data"`
 }
 
 type ErrorResponse struct {
