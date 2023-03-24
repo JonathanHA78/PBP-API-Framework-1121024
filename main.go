8ead7977e08baf6267f4553ddcee6c7d0fa27c3d
Jonathan_HA78 <joemaxwell789@gmail.com>
2023-03-24 13:51:59 +0700

    test4

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ package main
 // 		AllowMethods: []string{"PUT"},
 // 		AllowHeaders: []string{"Origin"},
 // 	})
-// 	m.Put("userscors/:user_id", allowCORSHandler, controller.UpdateUser)
+	m.Put("userscors/:user_id", allowCORSHandler, controller.UpdateUser)
 
 	m.Run()
 	m.RunOnAddr(":8181")
