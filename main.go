1e876c4262fec7f71b69e852350bfdbdf2b46499
Jonathan_HA78 <joemaxwell789@gmail.com>
2023-03-24 13:23:14 +0700

    test2 go build & test

--- main.go
+++ main.go
@@ -1,38 +1,31 @@
 package main
 
-import (
-	"Explore1/controller"
+// func main() {
 
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/cors"
-)
+// 	m := martini.Classic()
+// 	// 1 admin
+// 	// 2 users
+// 	m.Group("users", func(r martini.Router) {
+// 		r.Get("", controller.Authenticate(controller.GetAllUsers, 1))
+// 		r.Post("", controller.Authenticate(controller.InsertUser, 2))
+// 		r.Put("/:user_id", controller.UpdateUser)
+// 		r.Delete("/:user_id", controller.DeleteUser)
+// 	})
 
-func main() {
+// 	m.Use(cors.Allow(&cors.Options{
+// 		AllowOrigins:     []string{"localhost:8181"},
+// 		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE"},
+// 		AllowCredentials: true,
+// 	}))
+// 	// bisa juga digunakan seperti dibawah ini (untuk masing-masing endpoint)
+// 	allowCORSHandler := cors.Allow(&cors.Options{
+// 		AllowOrigins: []string{"localhost:8181"},
+// 		AllowMethods: []string{"PUT"},
+// 		AllowHeaders: []string{"Origin"},
+// 	})
+// 	m.Put("userscors/:user_id", allowCORSHandler, controller.UpdateUser)
 
-	m := martini.Classic()
-	// 1 admin
-	// 2 users
-	m.Group("users", func(r martini.Router) {
-		r.Get("", controller.Authenticate(controller.GetAllUsers, 1))
-		r.Post("", controller.Authenticate(controller.InsertUser, 2))
-		r.Put("/:user_id", controller.UpdateUser)
-		r.Delete("/:user_id", controller.DeleteUser)
-	})
+// 	// m.Run()
+// 	// m.RunOnAddr(":8181")
 
-	m.Use(cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"localhost:8181"},
-		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE"},
-		AllowCredentials: true,
-	}))
-	// bisa juga digunakan seperti dibawah ini (untuk masing-masing endpoint)
-	allowCORSHandler := cors.Allow(&cors.Options{
-		AllowOrigins: []string{"localhost:8181"},
-		AllowMethods: []string{"PUT"},
-		AllowHeaders: []string{"Origin"},
-	})
-	m.Put("userscors/:user_id", allowCORSHandler, controller.UpdateUser)
-
-	// m.Run()
-	// m.RunOnAddr(":8181")
-
-}
+// }
